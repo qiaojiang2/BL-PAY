dde54081c0868d2ec3533b67cab8367ab97be5b0
jiangqiao <jiangqiao@coolqi.com>
2017-08-07 17:55:57 +0800

    集成Saturn分布式定时任务系统

---
+++ bl-pay-base/bl-pay-example/src/main/java/cn/jishuyuan/pay/base/example/BlPayExampleApplication.java
@@ -0,0 +1,12 @@
+package cn.jishuyuan.pay.base.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlPayExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlPayExampleApplication.class, args);
+	}
+}
